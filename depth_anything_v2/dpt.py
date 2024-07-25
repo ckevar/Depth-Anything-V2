6b790cf22565587ff884ca1747828e6772c0ad3f
Christian Alvarado <ckevar@gmail.com>
2024-07-25 20:00:34 +0200

    Update dpt.py

--- depth_anything_v2/dpt.py
+++ depth_anything_v2/dpt.py
@@ -195,13 +195,13 @@ class DepthAnythingV2(nn.Module):
         
         ts = time.time()
         depth = F.interpolate(depth[:, None], (h, w), mode="bilinear", align_corners=True)
-        depth_cpu = depth.squeeze().cpu().numpy()
+        depth_cpu = depth.squeeze().cpu()
         #depth = (depth / torch.max(depth) * 255).type(torch.uint8)
         
         #depth = depth.cpu().numpy()
         ts = time.time() - ts
         print("Time on interpolate {}, type: {}".format(ts, type(depth)))
-        return depth_cpu
+        return depth_cpu.numpy()
     
     def image2tensor(self, raw_image, input_size=518):        
         '''

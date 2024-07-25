ec277a4138c5f7b7375233516ec65b7dcfdc3eba
Christian Alvarado <ckevar@gmail.com>
2024-07-25 22:29:12 +0200

    Update transform.py

--- depth_anything_v2/util/transform.py
+++ depth_anything_v2/util/transform.py
@@ -127,8 +127,8 @@ class Resize(object):
         
         # resize sample
         sample["image"] = cv2.resize(sample["image"][:,:,0], (width, height), interpolation=self.__image_interpolation_method)
-        print("Sample Image {}".format(sample["image"]))
-        cv2_imshow(sample["image"])
+        #print("Sample Image {}".format(sample["image"]))
+        #cv2_imshow(sample["image"])
         '''
         if self.__resize_target:
             if "depth" in sample:

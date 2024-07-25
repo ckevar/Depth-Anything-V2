a17860a92071f96cbf853c6b794e7466368fe39b
Christian Alvarado <ckevar@gmail.com>
2024-07-25 18:26:30 +0200

    Update dpt.py

--- depth_anything_v2/dpt.py
+++ depth_anything_v2/dpt.py
@@ -195,6 +195,7 @@ class DepthAnythingV2(nn.Module):
         ts = time.time()
         depth = F.interpolate(depth[:, None], (h, w), mode="bilinear", align_corners=True)[0, 0]
         depth = (depth / torch.max(depth) * 255).type(torch.uint8)
+        torch.cuda.synchronize()
         depth = depth.cpu().numpy()
         ts = time.time() - ts
         print("Time on interpolate {}, type: {}".format(ts, type(depth)))

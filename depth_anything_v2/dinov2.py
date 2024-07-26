62d0b8ce1b9233f98206d5b94bb9c5198359d135
Christian Alvarado <ckevar@gmail.com>
2024-07-26 09:44:20 +0200

    Update dinov2.py

--- depth_anything_v2/dinov2.py
+++ depth_anything_v2/dinov2.py
@@ -272,12 +272,19 @@ class DinoVisionTransformer(nn.Module):
     def _get_intermediate_layers_not_chunked(self, x, n=1):
         x = self.prepare_tokens_with_masks(x)
         # If n is an int, take the n last blocks. If it's a list, take them
-        output, total_block_len = [], len(self.blocks)
+        # OOUT: output, total_block_len = [], len(self.blocks)
+        total_block_len = len(self.blocks)
         blocks_to_take = range(total_block_len - n, total_block_len) if isinstance(n, int) else n
+        print("n {}, blocks_to_take {}, self.blocks {}".format(n, blocks_to_take, self.blocks))
+        output = [None] * len(n)
+        j = 0
         for i, blk in enumerate(self.blocks):
             x = blk(x)
             if i in blocks_to_take:
-                output.append(x)
+                # OOUT: output.append(x)
+                output[j] = x
+                j += 1
+            
         assert len(output) == len(blocks_to_take), f"only {len(output)} / {len(blocks_to_take)} blocks found"
         return output
 

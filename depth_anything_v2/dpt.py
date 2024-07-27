7680f614978d03af700672d888db86d586a89cf7
Christian Alvarado <ckevar@gmail.com>
2024-07-27 09:16:12 +0200

    Update dpt.py

--- depth_anything_v2/dpt.py
+++ depth_anything_v2/dpt.py
@@ -154,9 +154,9 @@ class DPTHead(nn.Module):
         path_1 = self.scratch.refinenet1(path_2, layer_1_rn)
         
         out = self.scratch.output_conv1(path_1)
-        print("out1 {}".format(out.size()))
-        out = F.interpolate(out, (int(patch_h * 14), int(patch_w * 14)), mode="bilinear", align_corners=True)
-        print("out2 {}".format(out.size()))
+        #print("out1 {}".format(out.size()))
+        #out = F.interpolate(out, (int(patch_h * 14), int(patch_w * 14)), mode="bilinear", align_corners=True)
+        #print("out2 {}".format(out.size()))
         out = self.scratch.output_conv2(out)
         
         return out
